cd1b8eccea5d555b3af0c2d1804ce0254da783c0
James Bayly <bayly.james@gmail.com>
2023-11-09 16:12:08 +0800

    Concordium mainnet

---
+++ Concordium/concordium-starter/project.ts
@@ -0,0 +1,90 @@
+import {
+  TransactionEventTag,
+  TransactionSummaryType,
+} from "@concordium/node-sdk";
+import {
+  ConcordiumDatasourceKind,
+  ConcordiumHandlerKind,
+  ConcordiumProject,
+} from "@subql/types-concordium";
+
+const project: ConcordiumProject = {
+  specVersion: "1.0.0",
+  name: "concordium-testnet-starter",
+  version: "0.0.1",
+  runner: {
+    node: {
+      name: "@subql/node-concordium",
+      version: "*",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  description:
+    "This project can be use as a starting point for developing your new Concordium SubQuery project",
+  repository: "https://github.com/subquery/concordium-subql-starter",
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /**
+     * chainId is the network identifier of the blockchain
+     * In Concordium it is always the genesis hash of the network (hash of the first block)
+     */
+    chainId: "4221332d34e1694168c2a0c0b3fd0f273809612cb13d000d5c2e00e85f50f796",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     */
+    endpoint: ["https://grpc.mainnet.concordium.software:20000"],
+  },
+  dataSources: [
+    {
+      kind: ConcordiumDatasourceKind.Runtime,
+      startBlock: 490000, // This changes your indexing start block, set this higher to skip initial blocks with less data
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          /**
+           * Avoid using block handlers where possible as they slow the indexing speed of your project
+          {
+            handler: "handleBlock",
+            kind: ConcordiumHandlerKind.Block,
+          },
+           */
+          {
+            handler: "handleTransaction",
+            kind: ConcordiumHandlerKind.Transaction,
+            filter: {
+              type: TransactionSummaryType.AccountTransaction,
+              values: {
+                transactionType: "transfer",
+              },
+            },
+          },
+          {
+            handler: "handleTransactionEvent",
+            kind: ConcordiumHandlerKind.TransactionEvent,
+            filter: {
+              type: TransactionEventTag.Updated,
+            },
+          },
+          {
+            handler: "handleSpecialEvent",
+            kind: ConcordiumHandlerKind.SpecialEvent,
+            filter: {
+              type: "blockAccrueReward",
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+// Must set default to the project instance
+export default project;
---
+++ Concordium/concordium-starter/src/mappings/mappingHandlers.ts
@@ -0,0 +1,62 @@
+import {
+  AccountTransactionSummary,
+  BlockSpecialEventBlockAccrueReward,
+  TransferSummary,
+  UpdatedEvent,
+} from "@concordium/node-sdk";
+import { Transfer, Updated, BlockAccrueReward } from "../types";
+import {
+  ConcordiumTransaction,
+  ConcordiumTransactionEvent,
+  ConcordiumSpecialEvent,
+} from "@subql/types-concordium";
+
+export async function handleTransaction(tx: ConcordiumTransaction) {
+  logger.info(
+    `Handling Transaction at block ${tx.block.blockHeight.toString()}`
+  );
+  const record = Transfer.create({
+    id: `${tx.block.blockHeight}-${tx.hash}`,
+    block: tx.block.blockHeight,
+    date: tx.block.blockReceiveTime,
+    from: (tx as AccountTransactionSummary).sender,
+    to: (tx as TransferSummary).transfer.to,
+    value: (tx as TransferSummary).transfer.amount,
+  });
+
+  await record.save();
+}
+
+export async function handleTransactionEvent(
+  txEvent: ConcordiumTransactionEvent
+) {
+  logger.info(
+    `Handling Transaction Event at block ${txEvent.block.blockHeight.toString()}`
+  );
+  const record = Updated.create({
+    id: `${txEvent.transaction.hash}-${txEvent.id}`,
+    block: txEvent.block.blockHeight,
+    txHash: txEvent.transaction.hash,
+    address: `${(txEvent as UpdatedEvent).address.subindex}-${
+      (txEvent as UpdatedEvent).address.subindex
+    }`,
+    instigator: (txEvent as UpdatedEvent).instigator.address.toString(),
+  });
+
+  await record.save();
+}
+
+export async function handleSpecialEvent(specialEvent: ConcordiumSpecialEvent) {
+  logger.info(
+    `Handling special event at block ${specialEvent.block.blockHeight.toString()}`
+  );
+  const record = BlockAccrueReward.create({
+    id: `${specialEvent.block.blockHeight}-${specialEvent.id}`,
+    block: specialEvent.block.blockHeight,
+    baker: (specialEvent as BlockSpecialEventBlockAccrueReward).baker,
+    bakerReward: (specialEvent as BlockSpecialEventBlockAccrueReward)
+      .bakerReward,
+  });
+
+  await record.save();
+}
